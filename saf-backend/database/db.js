771202dab972f5bdbc12fe8f275f826ec0c1dafa
jalmirsiqueira3 <162359129+jalmirsiqueira3@users.noreply.github.com>
2025-09-16 12:39:11 -0300

    Delete saf-backend directory

--- saf-backend/database/db.js
+++
@@ -1,9 +0,0 @@
-import sqlite3 from "sqlite3"
-import {open} from "sqlite"
-
-const dbPromise = open({
-    filename: './database/banco.db',
-    driver: sqlite3.Database
-});
-
-export const db = await dbPromise;
